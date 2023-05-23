23fa0b2a844c35f089871138c88b1418574f74ad
Andrew <a.lucas.webster@gmail.com>
2023-05-23 10:56:34 -0400

    added restart button

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
 /* eslint-disable no-trailing-spaces */
 /* eslint-disable prefer-const */
 
+// currently working on notes,
+// Need to remove the winner/tie message after hitting the restart button
+
 const player = (name, sign) => ({ name, sign });
 
 const gameFlow = (() => {
@@ -53,17 +56,20 @@ const displayControl = (() => {
     winnerBox.textContent = `${gameFlow.getActivePlayer().name} is the winner!`;
   };
 
+  const resetWinnerBox = () => {
+    winnerBox.textContent = '';
+  };
+
   const displayTie = () => {
     winnerBox.textContent = 'Its a tie!';
   };
 
-  // const disableBoard = () => {
-  //   const gameCells = document.querySelectorAll('.cell');
-  //   gameCells.forEach((item) => {
-  //   item.removeEventListener('click', () => {
-  //   }); 
-  // });
-  // };
+  const disableBoard = () => {
+    const gameCells = document.querySelectorAll('.cell');
+    gameCells.forEach((cell) => {
+      cell.setAttribute('disabled', true);
+  });
+  };
 
   let startButton = document.querySelector('.start');
   let startScreen = document.querySelector('.startScreen');
@@ -72,7 +78,9 @@ const displayControl = (() => {
     startScreen.classList.add('hidden');
     boardElement.classList.remove('hidden');
   });
-  return { displayWinner, displayTie };
+  return {
+ displayWinner, displayTie, disableBoard, resetWinnerBox, 
+};
 })();
 
 // Module for gameBoard
@@ -80,7 +88,7 @@ const gameBoard = (() => {
   let boardArray = ['', '', '', '', '', '', '', '', ''];
   let turn = 0; 
   const setBoard = () => {
-    const gameCells = document.querySelectorAll('.cell');
+    let gameCells = document.querySelectorAll('.cell');
     for (let i = 0; i < gameCells.length; i += 1) {
       gameCells[i].textContent = boardArray[i];
     }
@@ -95,21 +103,29 @@ const gameBoard = (() => {
       setBoard();
       if (gameFlow.checkWinner(boardArray)) {
         displayControl.displayWinner();
-        // displayControl.disableBoard();
+        displayControl.disableBoard();
       } else if (turn === 9) {
         displayControl.displayTie();
-        // displayControl.disableBoard();
+        displayControl.disableBoard();
       }
       gameFlow.switchPlayerTurn();
     }
   };
-
+// Empties array, clears board, and removes disabled property on clicking restart.
   let restartButton = document.querySelector('.restart');
     restartButton.addEventListener('click', () => {
       boardArray = ['', '', '', '', '', '', '', '', ''];
       setBoard();
+      let gameCells = document.querySelectorAll('.cell');
+      gameCells.forEach((cell) => {
+      if (cell.getAttribute('disabled', true)) {
+        cell.removeAttribute('disabled');
+      }
     });
-    
+      turn = 0;
+      displayControl.resetWinnerBox();
+    });
+
   // Listen for button click and determine which button was pressed passing that value to takeTurn
   const gameCells = document.querySelectorAll('.cell');
   gameCells.forEach((item) => {
@@ -120,6 +136,5 @@ const gameBoard = (() => {
   });
   return { setBoard };
 })();
-
 
 gameBoard.setBoard();
